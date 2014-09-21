2690c3911822f8d5b47b00a75b02e6d40291fed6
Rakesh.P.D <errakeshpd@gmail.com>
2014-09-21 11:11:05 +0530

    controller actions modified for adding files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,33 @@
 class ProductsController < ApplicationController
+    before_action :set_product, only: [:edit , :show, :destroy,:update]
+
+    def index
+        @products = Product.all
+    end
+    def show
+    end
+    def new
+        @product =Product.new
+    end
+    def edit
+    end
+    def create
+        @product = Product.new(product_params)
+        @product.save
+        redirect_to :action => :index
+    end
+    def destroy
+        @product.destroy
+    end
+
+
+    private
+    def set_product
+        @product = Product.find(params[:id])
+    end
+    def product_params
+        params.require(:product).permit(:title, :description, :image_url, :price)
+    end
+
+
 end
