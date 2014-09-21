71f31aa1bf9fb2cfc1484b114d3b4c091a6504a6
Rakesh.P.D <errakeshpd@gmail.com>
2014-09-21 16:23:46 +0530

    store controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     
+
    root 'products#index'
+   get 'store/index'
    
    resources :products
   # The priority is based upon order of creation: first created -> highest priority.
