b4cf35f94a717b97f1a30e09da20971439e94cd6
Rakesh.P.D <errakeshpd@gmail.com>
2014-09-21 16:31:51 +0530

    cart scaffolding done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920114817) do
+ActiveRecord::Schema.define(version: 20140921110044) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
